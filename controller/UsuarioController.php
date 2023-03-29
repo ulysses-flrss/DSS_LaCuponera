d1d628e249d59203f0b1395c03faa5692331d87a
DanielVasquez19 <danyy.vasz901@gmail.com>
2023-03-28 19:14:10 -0600

    Encriptar

--- controller/UsuarioController.php
+++ controller/UsuarioController.php
@@ -4,30 +4,31 @@ use UsuarioController as GlobalUsuarioController;
 
 use function PHPSTORM_META\map;
 
-include_once($_SERVER['DOCUMENT_ROOT'].'/DSS_LaCuponera/config.php');
-require_once(MODEL_PATH.'classUsuario.php');
-require_once(MODEL_PATH.'classValidaciones.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/DSS_LaCuponera/config.php');
+require_once(MODEL_PATH . 'classUsuario.php');
+require_once(MODEL_PATH . 'classValidaciones.php');
 
 
 
-class UsuarioController {
+class UsuarioController
+{
 
     private $usuario;
 
     public function __construct()
     {
         $this->usuario = new Usuario();
-        $accion = isset($_REQUEST['accion'])?$_REQUEST['accion']:'';
-        $redireccion = isset($_REQUEST['redireccion'])?$_REQUEST['redireccion']:'';
-        if($redireccion == "register") {
+        $accion = isset($_REQUEST['accion']) ? $_REQUEST['accion'] : '';
+        $redireccion = isset($_REQUEST['redireccion']) ? $_REQUEST['redireccion'] : '';
+        if ($redireccion == "register") {
             //var_dump($redireccion);
-            require_once(VIEW_PATH."viewRegister.php");
-        } else if($redireccion == "login") {
-            require_once(VIEW_PATH."viewLogin.php");
+            require_once(VIEW_PATH . "viewRegister.php");
+        } else if ($redireccion == "login") {
+            require_once(VIEW_PATH . "viewLogin.php");
         } else {
             if ($accion == '' || $accion == "logout") {
                 $this->index();
-            } else if ($accion == 'login') { 
+            } else if ($accion == 'login') {
                 $this->login();
             } else if ($accion == 'register') {
                 $this->register();
@@ -35,25 +36,27 @@ class UsuarioController {
         }
     }
 
-    public function index() {
-        require_once(VIEW_PATH."viewLogin.php");
+    public function index()
+    {
+        require_once(VIEW_PATH . "viewLogin.php");
     }
 
-    public function login () {
-        $correo = isset($_POST['correo'])?$_POST['correo']:'';
-        $password = isset($_POST['password'])?$_POST['password']:'';
-        
+    public function login()
+    {
+        $correo = isset($_POST['correo']) ? $_POST['correo'] : '';
+        $password = isset($_POST['password']) ? $_POST['password'] : '';
+
         $this->usuario->setCorreo($correo);
         $this->usuario->setPassword($password);
-        
+
         $result = $this->usuario->validarCorreoPassword($this->usuario->getCorreo(), $this->usuario->getPassword());
         if ($result == "OK") {
             // Redireccion a Pagina Index
-            require_once(VIEW_PATH.'viewOfertas.php');
+            require_once(VIEW_PATH . 'viewOfertas.php');
         } else {
             // Mostrar mensaje de error de autentificación 
-            var_dump($result);  
-            require_once(VIEW_PATH.'viewLogin.php');
+            var_dump($result);
+            require_once(VIEW_PATH . 'viewLogin.php');
 
             // echo "<script>
             // Swal.fire({
@@ -62,26 +65,45 @@ class UsuarioController {
             //     text: 'Something went wrong!',  
             //   })
             // </script>";
-        }        
+        }
     }
 
-    public function register() {
-        $correo = isset($_POST['correo'])?$_POST['correo']:'';
-        $password = isset($_POST['password'])?$_POST['password']:'';
-        $passwordConf = isset($_POST['passwordConfirm'])?$_POST['passwordConfirm']:'';
-        $dui = isset($_POST['dui'])?$_POST['dui']:'';
-        $nombre = isset($_POST['nombre'])?$_POST['nombre']:'';
-        $apellido = isset($_POST['apellido'])?$_POST['apellido']:'';
-        $telefono = isset($_POST['telefono'])?$_POST['telefono']:'';
-        $codEmpresa = isset($_POST['codEmpresa'])?$_POST['codEmpresa']:'';
-        $codRol = isset($_POST['codRol'])?$_POST['codRol']:'';
- 
+    public function register()
+    {
+        $correo = isset($_POST['correo']) ? $_POST['correo'] : '';
+        $password = isset($_POST['password']) ? $_POST['password'] : '';
+        $passwordConf = isset($_POST['passwordConfirm']) ? $_POST['passwordConfirm'] : '';
+        $dui = isset($_POST['dui']) ? $_POST['dui'] : '';
+        $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+        $apellido = isset($_POST['apellido']) ? $_POST['apellido'] : '';
+        $telefono = isset($_POST['telefono']) ? $_POST['telefono'] : '';
+        $codEmpresa = isset($_POST['codEmpresa']) ? $_POST['codEmpresa'] : '';
+        $codRol = isset($_POST['codRol']) ? $_POST['codRol'] : '';
+        //Token del correo, a saber si sirve bro xd
+        //$token = 'VERIFY_' . uniqid();
+        //$to = $correo;
+        //$titulo = 'Verificación de cuenta';
+        //$mensaje = 'Su token de verificación es: ' . $token;
+        //$url = 'Desde: http://localhost/DSS_LaCuponera/index.php' . "\r\n" . phpversion();
+
+        //mail($to, $titulo, $mensaje, $url);
+
+        //$query = "SELECT * FROM usuarios WHERE token_verificacion = '$token'";
+        //$result = mysqli_query($conexion, $query);
+
+        //if (mysqli_num_rows($result) == 1) {
+            // Token válido, activar cuenta y redirigir al usuario
+        //} else {
+            // Token inválido, mostrar mensaje de error
+        //}
 
         $this->usuario->setCorreo($correo);
 
         if ($password === $passwordConf) {
-            //Aqui pone el codigo para encriptar la contraseña
-            $this->usuario->setPassword($password);
+            $salt = password_hash($password, PASSWORD_DEFAULT);
+            $hashed_password = password_hash($password, PASSWORD_BCRYPT, ['salt' => $salt]);
+            echo $hashed_password;
+            $this->usuario->setPassword($hashed_password);
         } else {
             return "CONTRASEÑAS DIFERENTES";
         }
@@ -105,10 +127,10 @@ class UsuarioController {
                 null,
                 $this->usuario->getCodRol()
             );
-        } catch(Exception $e) {
+        } catch (Exception $e) {
             echo "
                 <script>
-                    alert(".$e->getMessage().")
+                    alert(" . $e->getMessage() . ")
                 </script>
             ";
         }
@@ -116,24 +138,18 @@ class UsuarioController {
 
         if ($result == "OK") {
             // Redireccion a Pagina Index
-            require_once(VIEW_PATH.'viewLogin.php');
+            require_once(VIEW_PATH . 'viewLogin.php');
         } else {
             // Mostrar mensaje de error de autentificación   
-            var_dump($result);  
-            require_once(VIEW_PATH.'viewRegister.php');
-        }  
+            var_dump($result);
+            require_once(VIEW_PATH . 'viewRegister.php');
+        }
     }
 
-    public function logout () {
+    public function logout()
+    {
         session_start();
         session_destroy();
-        require_once(VIEW_PATH.'viewLogin.php');
+        require_once(VIEW_PATH . 'viewLogin.php');
     }
-
 }
-
-    
-    
-    
-
-?>
