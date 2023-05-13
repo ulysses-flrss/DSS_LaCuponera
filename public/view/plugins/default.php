5a5314b3fe0431df209db11e6f34f04fe22da741
ulysses-flrss <ulisesf3136@gmail.com>
2023-05-12 21:52:21 -0600

    Co-authored-by: Daniel Vásquez <DanielVasquez19@users.noreply.github.com>

--- public/view/plugins/default.php
+++ public/view/plugins/default.php
@@ -1,16 +1,21 @@
 <?php 
 include_once($_SERVER['DOCUMENT_ROOT'].'/DSS_LaCuponera/public/config.php');
 function head() {
-    return <<<EOD
-        <link rel="stylesheet" href="view/css/root-style.css" crossorigin="anoymous">
-        <link rel="stylesheet" href="view/css/normalize.css" crossorigin="anoymous">
-        <link rel="stylesheet" href="view/css/menu-style.css" crossorigin="anoymous">
+    $relativePath = dirname($_SERVER['PHP_SELF']);
+    $head = <<<EOD
+        <link rel="stylesheet" href="$relativePath/view/css/root-style.css" crossorigin="anoymous">
+        <link rel="stylesheet" href="$relativePath/view/css/view/css/normalize.css" crossorigin="anoymous">
+        <link rel="stylesheet" href="$relativePath/view/css/view/css/menu-style.css" crossorigin="anoymous">
         <link rel="stylesheet" href="https://kit.fontawesome.com/b5142e7f7e.css" crossorigin="anoymous">
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script> 
         <script src="sweetalert2.min.js"></script>
         <link rel="stylesheet" href="sweetalert2.min.css">
         EOD;
+
+    $head = str_replace('CSS_PATH', CSS_PATH, $head);
+    
+    return $head;
 }
 
 function menu() {
