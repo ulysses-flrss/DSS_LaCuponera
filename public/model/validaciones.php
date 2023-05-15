6ed5d7743eae04371a5beb04305c9bb8cc152744
AndresVR04 <90435994+AndresVR04@users.noreply.github.com>
2023-05-15 00:19:26 -0600

    Add files via upload

--- public/model/validaciones.php
+++ public/model/validaciones.php
@@ -72,6 +72,32 @@ class Validacion {
             "El formato del codigo de empresa es incorrecto";
         }
     }
+
+    public static function isNumTarjeta($numTarjeta) {
+        $regExp = "/^(4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13})$/";
+        if (preg_match($regExp, $numTarjeta)) {
+            return "OK";
+        } else {
+            "El formato del numero de tarjeta es incorrecto";
+        }
+    }
+    
+    public static function isFechaExpi($fecha) {
+        $regExp = "/^(0[1-9]|1[0-2])\/\d{2}$/";
+        if (preg_match($regExp, $fecha)) {
+            return "OK";
+        } else {
+            "El formato de fecha es incorrecto";
+        }
+    }
+    public static function isCVV($cvv) {
+        $regExp = "/^\d{3}$/";
+        if (preg_match($regExp, $cvv)) {
+            return "OK";
+        } else {
+            "El formato del numero de tarjeta es incorrecto";
+        }
+    }
     
 }
 
